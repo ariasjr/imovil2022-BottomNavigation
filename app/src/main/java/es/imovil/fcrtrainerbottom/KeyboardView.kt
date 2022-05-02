a6f3cce7c208afd49679f59c4da6318b06b55c25
sergioandresestrada <64619915+sergioandresestrada@users.noreply.github.com>
2022-05-02 13:25:41 +0200

    solved shared files conflicts

--- app/src/main/java/es/imovil/fcrtrainerbottom/KeyboardView.kt
+++ app/src/main/java/es/imovil/fcrtrainerbottom/KeyboardView.kt
@@ -12,6 +12,7 @@ import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.Button
+import es.imovil.fcrtrainerbottom.databinding.FragmentDigitalsOperacionLogicaBinding
 import es.imovil.fcrtrainerbottom.databinding.KeyboardFullPanelBinding
 
 /**
